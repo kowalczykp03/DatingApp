766e7dc46703201b4b3ea772b14462ea5a3776d2
kowalczykp03 <kowalczykp.dev@gmail.com>
2023-08-01 22:38:16 +0200

    Add migration and UserController

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -19,8 +19,8 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
-            var users = await _context.Users.ToListAsync();
-            return users;
+            return await _context.Users.ToListAsync();
+            
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUser(int id)
